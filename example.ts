8b3872242deb05338ccb865ce35cfdfef7a4ebbd
spleekz <spleekz@zohomail.com>
2023-07-01 16:07:56 +0300

    initial

---
+++ example.ts
@@ -0,0 +1,30 @@
+var num: number = 31
+
+var str: string = 'goth'
+
+var bool: boolean = true
+
+var obj = {
+	a: 1,
+	b: 2,
+}
+
+function fn( param: { value: any } ) {
+	console.log( param.value )
+}
+
+
+var interpolatedString = `random: ${ Math.random() }`
+
+
+var error = obj.wrongName
+
+
+// comment
+
+
+// find - match
+
+console.log( 'gothic' )
+
+var isGothic = true
